5dd8e1b31f06ce29d91c1c61d676d338b71b2424
Umutoni26 <vanessaumutoni26@gmail.com>
2023-12-07 16:34:42 -0800

    java user registration project

---
+++ registration_final/src/PreraredStatement.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+class PreraredStatement {
+
+    void setString(int PROPERTIES, String query) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void executeUpdate() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
